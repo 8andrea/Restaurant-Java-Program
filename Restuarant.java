07ea5f7b137bc20f28a152f87bf00ebe0d1eb6c8
8andrea <andreadelgado0616@gmail.com>
2026-01-28 13:33:40 -0700

    Add files via upload

---
+++ Restuarant.java
@@ -0,0 +1,24 @@
+public class Restuarant {
+
+    // Properties
+    int waitTime;
+    double rating;
+    double avgPrice;
+    double distance;
+    String hoursOpen;
+
+    // Constructor
+
+    public Restuarant() {}
+
+    public Restuarant(int waitTime, double rating, double avgPrice, double distance, String hoursOpen) {
+        this.waitTime = waitTime;
+        this.rating = rating;
+        this.avgPrice = avgPrice;
+        this.distance = distance;
+        this.hoursOpen = hoursOpen;
+
+
+    }
+
+}
